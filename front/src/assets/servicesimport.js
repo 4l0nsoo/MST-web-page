6e124a0129a82f100464f292a58822c8a1c5f37a
Alonso <di4zalonsoj@gmail.com>
2025-10-24 12:56:57 -0500

    avance 24/10 12:56

--- front/src/assets/servicesimport.js
+++ front/src/assets/servicesimport.js
@@ -8,13 +8,14 @@ import arduino2 from "./fotos-mantenimiento/arduino2.png"
 import team1 from "./fotos-elementos-empresa/mantenimiento.jpeg"
 import logo from "./logo.png"
 import fullteam from "./fullteam.jpeg"
+import plano from "./plano empresa.png"
 import imgexmpl from './exampleimg.jpg'
 
 export const mantenimiento = [mant1, mant2, mant3];
 export const redes = [red1, red2];
 export const arduino = [arduino1, arduino2];
 export const tecnicos = [team1];
-export const aboutStart = [fullteam, logo];
+export const aboutStart = [fullteam, logo, plano];
 export const tecnicosRedes = [];
 export const tecnicosArduino = [];
 export const exmpl = imgexmpl
--- front/src/components/AboutUs.jsx
+++ front/src/components/AboutUs.jsx
@@ -36,6 +36,11 @@ function AboutUs() {
     </ol>
   </section>
 
+    <div className='imgPlano'>
+      <h1>Plano de la empresa</h1>
+      <img src={aboutStart[2]} alt="" />
+    </div>
+
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1408.1057129130304!2d-76.50085657622134!3d3.4171875079507017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1756574554212!5m2!1ses-419!2sco" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
