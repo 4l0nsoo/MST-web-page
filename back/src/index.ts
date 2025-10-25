e68ec2b33f805511d334abf75b8329c1860bc78c
Alonso <di4zalonsoj@gmail.com>
2025-10-24 22:35:57 -0500

    avance 24/19 10:35pm

--- back/src/index.ts
+++ back/src/index.ts
@@ -9,11 +9,19 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://mst-web-page.vercel.app/"],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api/mail", mailRoutes);
 app.use("/api", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API funcionando correctamente");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
