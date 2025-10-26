aa49884a3cda1f8c0a58978ee8c4874aed0621c0
Alonso <di4zalonsoj@gmail.com>
2025-10-26 00:39:37 -0500

    chatbot try4

--- back/src/index.ts
+++ back/src/index.ts
@@ -4,32 +4,35 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth";
 import mailRoutes from "./routes/mail";
-import bodyParser from "body-parser";
 import chatbotRoutes from "./routes/chatbot";
-
-
 
 const app = express();
 
+// Middleware de logging para depuración
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
 
-// Desarrollo
-// app.use(cors());
-
-// Produccion
 app.use(express.json());
+
 app.use(cors({
   origin: ["https://mst-web-page.vercel.app"],
-  methods: ["GET", "POST","OPTIONS"],
+  methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
 }));
+
+// Rutas
 app.use("/api/mail", mailRoutes);
 app.use("/api", authRoutes);
 app.use("/api", chatbotRoutes);
 
+// Ruta raíz
 app.get("/", (req, res) => {
   res.send("API funcionando correctamente");
 });
 
+// Puerto dinámico para Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
